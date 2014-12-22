1a866029723a2441bc87fc972cc3e47e56080636
卢键 <lujian@19lou.com>
2014-12-22 12:40:59 +0800

    init

--- README.md
+++ README.md
@@ -1,31 +1,61 @@
-weixin
-======
-weixin sdk
+yii2-wechat
+===============
 
-Installation
+
+环境条件
+------------
+- >= php5.4
+- Yii2
+
+安装
 ------------
 
-The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+您可以使用composer来安装, 添加下列代码在您的``composer.json``文件中并执行``composer update``操作
 
-Either run
-
-```
-php composer.phar require --prefer-dist weixin/yii2-weixin "*"
-```
-
-or add
-
-```
-"weixin/yii2-weixin": "*"
+```json
+{
+    "require": {
+       "lujian/yii2-wechat": "dev-master"
+    }
+}
 ```
 
-to the require section of your `composer.json` file.
+使用示例
+------------
+在使用前,请先参考微信公众平台的[开发文档](http://mp.weixin.qq.com/wiki/index.php?title=%E9%A6%96%E9%A1%B5)
 
-
-Usage
------
-
-Once the extension is installed, simply use it in your code by  :
-
+Wechat定义方式
 ```php
-<?= \weixin\Wechat::widget(); ?>```
+//在config/web.php配置文件中定义component配置信息
+'components' => [
+  .....
+  'wechat' => [
+    'class' => 'lujian\wechat\Wechat',
+    'config' => [
+        'token'=>'tokenaccesskey', //填写你设定的key
+        'encodingaeskey'=>'encodingaeskey', //填写加密用的EncodingAESKey
+        'appid'=>'wxdk1234567890', //填写高级调用功能的app id, 请在微信开发模式后台查询
+        'appsecret'=>'xxxxxxxxxxxxxxxxxxx', //填写高级调用功能的密钥
+        'partnerid'=>'88888888', //财付通商户身份标识，支付权限专用，没有可不填
+        'partnerkey'=>'', //财付通商户权限密钥Key，支付权限专用
+        'paysignkey'=>'' //商户签名密钥Key，支付权限专用
+    ]
+  ]
+  ....
+]
+// 全局公众号sdk使用
+$wechat = Yii::$app->wechat;
+
+
+
+
+```
+
+反馈或贡献代码
+------------
+您可以在[这里](https://github.com/lujian/yii2-wechat/issues)给我提出在使用中碰到的问题或Bug.
+我会在第一时间回复您并修复.
+
+您也可以 发送邮件254461627@qq.com给我并且说明您的问题.
+
+如果你有更好代码实现,请fork项目并发起您的pull request.我会及时处理. 感谢!
--- Wechat.php
+++ Wechat.php
@@ -1,52 +1,9 @@
 <?php
 
-namespace weixin;
-/**
- *	微信公众平台PHP-SDK, 官方API部分
- *  @author  dodge <dodgepudding@gmail.com>
- *  @link https://github.com/dodgepudding/wechat-php-sdk
- *  @version 1.2
- *  usage:
- *   $options = array(
- *			'token'=>'tokenaccesskey', //填写你设定的key
- *			'encodingaeskey'=>'encodingaeskey', //填写加密用的EncodingAESKey
- *			'appid'=>'wxdk1234567890', //填写高级调用功能的app id
- *			'appsecret'=>'xxxxxxxxxxxxxxxxxxx', //填写高级调用功能的密钥
- *			'partnerid'=>'88888888', //财付通商户身份标识
- *			'partnerkey'=>'', //财付通商户权限密钥Key
- *			'paysignkey'=>'' //商户签名密钥Key
- *		);
- *	 $weObj = new Wechat($options);
- *   $weObj->valid();
- *   $type = $weObj->getRev()->getRevType();
- *   switch($type) {
- *   		case Wechat::MSGTYPE_TEXT:
- *   			$weObj->text("hello, I'm wechat")->reply();
- *   			exit;
- *   			break;
- *   		case Wechat::MSGTYPE_EVENT:
- *   			....
- *   			break;
- *   		case Wechat::MSGTYPE_IMAGE:
- *   			...
- *   			break;
- *   		default:
- *   			$weObj->text("help info")->reply();
- *   }
- *
- *   //获取菜单操作:
- *   $menu = $weObj->getMenu();
- *   //设置菜单
- *   $newmenu =  array(
- *   		"button"=>
- *   			array(
- *   				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
- *   				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
- *   				)
- *  		);
- *   $result = $weObj->createMenu($newmenu);
- */
-class Wechat extends \yii\base\Widget
+namespace lujian\wechat;
+
+
+class Wechat extends yii\base\Component
 {
     const MSGTYPE_TEXT = 'text';
     const MSGTYPE_IMAGE = 'image';
@@ -119,7 +76,8 @@ class Wechat extends \yii\base\Widget
     public $errMsg = "no access";
     private $_logcallback;
 
-    public function __construct($options)
+
+    public function setConfig($options)
     {
         $this->token = isset($options['token'])?$options['token']:'';
         $this->encodingAesKey = isset($options['encodingaeskey'])?$options['encodingaeskey']:'';
